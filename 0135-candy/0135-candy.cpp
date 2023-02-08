c34d7c79ea296625b8ecfcbdd68d77a9bde4bc9b
NIKHIL UPADHYAY <87298477+uniiik@users.noreply.github.com>
2023-02-09 00:21:25 +0530

    Time: 26 ms (54.98%), Space: 17.8 MB (41.38%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -38,3 +38,9 @@ public:
         return cnt;
     }
 };
+
+/*
+first initialze  a vector candies and assign 1 as initial value as it is min condn
+now traverse loop from left to right and if rating of ith is greater than i-1 th and no candies at index i is less than or equal to i-1 th index then increase count in vector
+now again traverse from right to left and do same process 
+*/
