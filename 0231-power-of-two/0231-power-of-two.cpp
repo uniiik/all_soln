90f4c780df3525c3498854f3bf534bacf94de3ad
NIKHIL UPADHYAY <87298477+uniiik@users.noreply.github.com>
2023-02-14 23:09:58 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (62.34%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<=0)
+            return false;
+        
+       return ((n&(n-1))==0);
+        
+    }
+};
+
+/*
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n==0)
+            return false;
+        
+       while(n%2==0)
+           n=n/2;
+        
+        if(n==1)
+            return true;
+        else 
+            return false;
+    }
+};
+
+*/
