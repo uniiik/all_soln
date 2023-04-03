ad72dd537ae3b17c84515851b2b8c3db5b069dc2
NIKHIL UPADHYAY <87298477+uniiik@users.noreply.github.com>
2023-04-03 16:49:38 +0530

    Time: 88 ms (62.35%), Space: 42 MB (11.29%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -26,3 +26,6 @@ public:
         
     }
 };
+/*
+we have used 2 pointer approach in this first we sorted the array and then we start traversing if the sum of lightest and heaviest is under limit then we will add it to the count other wise only heaviest will go 
+*/
