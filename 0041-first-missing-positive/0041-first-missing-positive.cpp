6b9aee7d2b26df4dd047366dde515bccb7406825
NIKHIL UPADHYAY <87298477+uniiik@users.noreply.github.com>
2023-04-07 00:01:10 +0530

    Time: 57 ms (62.58%), Space: 41.2 MB (35.04%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,23 +1,41 @@
 class Solution {
 public:
-    int firstMissingPositive(vector<int>& nums) {
-        
-        unordered_map<int,int> umap;
-        int max=*max_element(nums.begin(),nums.end());
-        for(auto x:nums)
-            umap[x]++;
-        
-        
-        if(max<0)
-            return 1;
-        
-        for(int i=1;i<max;i++)
-        {
-            if(umap[i]==0)
-                return i;
-            
+int firstMissingPositive(vector<int>& nums) {
+    int n = nums.size();
+    bool flag = false;
+
+    // Check if 1 is present in the array or not
+    for (int i = 0; i < n; i++) {
+        if (nums[i] == 1) {
+            flag = true;
+            break;
         }
-        return max+1;
-        
     }
+    if (!flag) {
+        return 1;
+    }
+
+    // Mark non-positive and out-of-range elements with a special value (e.g., 1)
+    for (int i = 0; i < n; i++) {
+        if (nums[i] <= 0 || nums[i] > n) {
+            nums[i] = 1;
+        }
+    }
+
+    // Use the array indices as a hash table to mark present elements
+    for (int i = 0; i < n; i++) {
+        int index = abs(nums[i]) - 1;
+        if (nums[index] > 0) {
+            nums[index] = -nums[index];
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        if (nums[i] > 0) {
+            return i + 1;
+        }
+    }
+
+    return n + 1;
+}
+
 };
