0cdf7306284a598631b7265090d3f193978e2448
NIKHIL UPADHYAY <87298477+uniiik@users.noreply.github.com>
2023-04-13 00:05:01 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (10.15%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     int fib(int n) {
+         vector<int> array(n+1);
         
-        if(n<=1)
-        {
+        if(n<2)
             return n;
-            
+        
+        array[0]=0;
+        array[1]=1;
+      
+        for(int i=2;i<=n;i++)
+        {
+            array[i]=array[i-1]+array[i-2];
         }
-
-
-
-
-        return fib(n-1)+fib(n-2);
+        
+        
+        return array[n];
         
     }
 };
