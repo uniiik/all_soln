7835e6db8b0bf967930d400272609e5e4fb58b01
NIKHIL UPADHYAY <87298477+uniiik@users.noreply.github.com>
2023-09-28 00:52:26 +0530

    Time: 86 ms (5.07%), Space: 134.6 MB (5.15%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,27 +1,47 @@
 class Solution {
 public:
-    void dfs(int node, vector<int>& visited, vector<vector<int>>& isConnected) {
-        visited[node] = 1; // Mark the node as visited
-
-        for (int neighbor = 0; neighbor < isConnected.size(); neighbor++) {
-            if (isConnected[node][neighbor] == 1 && visited[neighbor] == 0) {
-                dfs(neighbor, visited, isConnected);
-            }
-        }
+    void dfs(int node, vector<int> &visited,vector<vector<int>>isConnected){
+        visited[node]=1;//mark the number as visited
+            
+            
+       for(int i=0;i<isConnected[0].size();i++)//checking all the neighbours of that particular node
+       {
+            if(isConnected[i][node]==1 &&   visited[i]==0){
+                visited[i]=1;
+                dfs(i,visited,isConnected);}
+        }  
+    
+    
+    
+    
+    
+    
     }
-
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
-        vector<int> visited(n, 0);
-        int count = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (visited[i] == 0) {
-                dfs(i, visited, isConnected);
-                count++;
-            }
+        
+        int count=0;
+        int n=isConnected[0].size();
+        vector<int> visited(n,0);
+        for(int i=0;i<n;i++){
+            
+            if(visited[i]==0){
+                dfs(i,visited,isConnected);
+                count++;}
+            
         }
-
+        
+        
         return count;
+        
+        
+        
     }
 };
+/*
+so mainly here we are first running a for loop form i =0 to i< isConnected[0].size().....
+and we are checking whether the index position in visited array is 0 or 1
+if the position is indicationg 0 means it has not been visited once so we will just call dfs on that node
+now dfs call we do its work and check all the connected elemnts to it
+once all the connected elemnts to that node has been completed then we will just increase the counter variable by 1
+and in last return count
+*/
