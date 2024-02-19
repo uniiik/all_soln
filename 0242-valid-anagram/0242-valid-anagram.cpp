00500c10efe4818f8c1baa104169a3e95696a495
NIKHIL UPADHYAY <87298477+uniiik@users.noreply.github.com>
2024-02-19 21:56:05 +0530

    Time: 6 ms (81.11%), Space: 9.6 MB (15.93%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+        unordered_map<char,int> map1;
+        unordered_map<char,int> map2;
+        for(int i=0;i<s.size();i++){
+            map1[s[i]]++;
+        }
+        for(int j=0;j<t.size();j++){
+            map2[t[j]]++;
+        }
+        return map1==map2;
         
     }
 };
